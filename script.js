abec849d788765369a1dc357cfc985076fc640d0
ankitapandey <ankitapandey.bgr2005@gmail.com>
2025-08-24 20:20:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function filterVideos(category) {
+  let cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    if (category === 'all' || card.classList.contains(category)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
